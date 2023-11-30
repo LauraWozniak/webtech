cae24427c8a5138aceb4c649f058582187449c77
Laura Wozniak <Laura.Wozniak@Student.HTW-Berlin.de>
2023-11-30 19:49:16 +0100

    bugfix für JSON fehler

--- src/main/java/com/example/demo/springboot/web/api/web/api/ErinnerungRestController.java
+++ src/main/java/com/example/demo/springboot/web/api/web/api/ErinnerungRestController.java
@@ -5,6 +5,7 @@ import com.example.demo.springboot.web.api.web.ErinnerungmanipulationRequest;
 import com.example.demo.springboot.web.api.web.api.Erinnerung;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +23,7 @@ public class ErinnerungRestController {
     public ErinnerungRestController() {
 
     }
-    @GetMapping(path = "/api/v1/erinnerungen")
+    @GetMapping(path = "/api/v1/erinnerungen", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Erinnerung> fetchErinnerungen(){
         return service.getAll();
     }
